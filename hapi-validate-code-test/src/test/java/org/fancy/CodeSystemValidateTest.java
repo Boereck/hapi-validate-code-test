1baae6c33f7c7e443786c82506aad7f235396e2b
Max Bureck <max.bureck@fokus.fraunhofer.de>
2021-03-15 18:10:19 +0100

    Create simple test for $validate-code on CodeSystem

---
+++ hapi-validate-code-test/src/test/java/org/fancy/CodeSystemValidateTest.java
@@ -0,0 +1,112 @@
+package org.fancy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.InputStream;
+
+import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.instance.model.api.IIdType;
+import org.hl7.fhir.r4.model.CodeSystem;
+import org.hl7.fhir.r4.model.CodeType;
+import org.hl7.fhir.r4.model.IdType;
+import org.hl7.fhir.r4.model.Parameters;
+import org.junit.jupiter.api.Test;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
+
+class CodeSystemValidateTest {
+
+  private static final String SERVER_BASE = "http://localhost:8080/fhir";
+  private static final String CODE_SYSTEM_PATH = "/fancy.json";
+
+  @Test()
+  void testOnValidationOnInstance() {
+
+    FhirContext context = FhirContext.forR4();
+
+    IGenericClient client = createClient(context);
+    IParser jsonParser = context.newJsonParser().setPrettyPrint(true);
+
+    CodeSystem codeSystem = loadTestResource(context);
+
+    MethodOutcome updateOutcome = client.update().resource(codeSystem).execute();
+    // MethodOutcome updateOutcome = client.create().resource(codeSystem).execute();
+    IBaseResource updateResultResource = updateOutcome.getResource();
+
+    String updateResultStr = jsonParser.encodeResourceToString(updateResultResource);
+    System.err.println(updateResultStr);
+
+    Parameters parameters = new Parameters().addParameter("code", new CodeType("fancy-two"));
+
+    IdType codeSystemId = codeSystem.getIdElement().toUnqualifiedVersionless();
+    // IIdType codeSystemId = updateResultResource.getIdElement();
+    Parameters validationResult =
+        client
+            .operation()
+            .onInstance(codeSystemId)
+            .named("validate-code")
+            .withParameters(parameters)
+            .execute();
+
+    System.err.println(jsonParser.encodeResourceToString(validationResult));
+
+    boolean result = validationResult.getParameterBool("result");
+    assertTrue(result);
+  }
+
+  @Test
+  void testOnValidationOnType() {
+
+    FhirContext context = FhirContext.forR4();
+
+    IGenericClient client = createClient(context);
+    IParser jsonParser = context.newJsonParser().setPrettyPrint(true);
+
+    CodeSystem codeSystem = loadTestResource(context);
+
+    MethodOutcome updateOutcome = client.update().resource(codeSystem).execute();
+    //    MethodOutcome updateOutcome = client.create().resource(codeSystem).execute();
+    IBaseResource updateResultResource = updateOutcome.getResource();
+
+    String updateResultStr = jsonParser.encodeResourceToString(updateResultResource);
+    System.err.println(updateResultStr);
+
+    Parameters parameters =
+        new Parameters()
+            .addParameter("url", codeSystem.getUrlElement())
+            .addParameter("code", new CodeType("fancy-two"));
+
+    Parameters validationResult =
+        client
+            .operation()
+            .onType(CodeSystem.class)
+            .named("validate-code")
+            .withParameters(parameters)
+            .execute();
+
+    System.err.println(jsonParser.encodeResourceToString(validationResult));
+
+    boolean result = validationResult.getParameterBool("result");
+    assertTrue(result);
+  }
+
+  private IGenericClient createClient(FhirContext context) {
+    IGenericClient client = context.newRestfulGenericClient(SERVER_BASE);
+
+    LoggingInterceptor loggingInterceptor = new LoggingInterceptor();
+    loggingInterceptor.setLogRequestSummary(true);
+    loggingInterceptor.setLogRequestBody(false);
+    client.registerInterceptor(loggingInterceptor);
+    return client;
+  }
+
+  private CodeSystem loadTestResource(FhirContext context) {
+    IParser jsonParser = context.newJsonParser();
+    InputStream resourceAsStream = getClass().getResourceAsStream(CODE_SYSTEM_PATH);
+    return jsonParser.parseResource(CodeSystem.class, resourceAsStream);
+  }
+}
